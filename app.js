c28ebecd0bfafa93b47bfbe82243fd7484f05d60
julian257 <julian.anguiano23b@udgvirtual.udg.mx>
2025-11-17 11:42:13 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const API = "https://api-tareas-production.up.railway.app/";
+
+// Cargar tareas
+async function cargarTareas() {
+    const res = await fetch(API);
+    const data = await res.json();
+
+    let html = "";
+    data.forEach(t => {
+        html += `
+            <div>
+                <b>${t.titulo}</b> - ${t.descripcion}
+                <button onclick="eliminarTarea(${t.id})">Eliminar</button>
+            </div>
+        `;
+    });
+
+    document.getElementById("tareas").innerHTML = html;
+}
+
+// Crear tarea
+async function crearTarea() {
+    const titulo = document.getElementById("titulo").value;
+    const descripcion = document.getElementById("descripcion").value;
+
+    await fetch(API, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ titulo, descripcion })
+    });
+
+    cargarTareas();
+}
+
+// Eliminar tarea
+async function eliminarTarea(id) {
+    await fetch(`${API}/${id}`, {
+        method: "DELETE"
+    });
+
+    cargarTareas();
+}
+
+cargarTareas();
